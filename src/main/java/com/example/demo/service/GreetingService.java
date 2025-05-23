809b1629a97d60f31d8b3c25d89f3cf632302a86
Vishal Umasankar <vishalumasankar@gmail.com>
2025-05-23 12:49:26 +0530

    Added UC1 Mapping methods

--- src/main/java/com/example/demo/service/GreetingService.java
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -15,10 +15,6 @@ public class GreetingService {
     @Autowired
     private GreetingRepository greetingRepository;
 
-    public Greeting save(Greeting greeting) {
-        return greetingRepository.save(greeting);
-    }
-
     public Greeting getGreetingMessage(String type) {
         switch (type.toLowerCase()) {
             case "post":
@@ -30,45 +26,5 @@ public class GreetingService {
             default:
                 return new Greeting(0, "Hello from GET (via Service)");
         }
-    }
-
-    public Greeting getPersonalizedGreeting(String firstName, String lastName) {
-        String message;
-
-        if (firstName != null && lastName != null) {
-            message = "Hello, " + firstName + " " + lastName + "!";
-        } else if (firstName != null) {
-            message = "Hello, " + firstName + "!";
-        } else if (lastName != null) {
-            message = "Hello, Mr/Ms. " + lastName + "!";
-        } else {
-            message = "Hello World";
-        }
-
-        return new Greeting(0, message);
-    }
-
-    public Greeting findById(long id) {
-        return greetingRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Greeting not found"));
-    }
-
-    public List<Greeting> findAll() {
-        return greetingRepository.findAll();
-    }
-
-    public Greeting update(long id, Greeting greeting) {
-        if (!greetingRepository.existsById(id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Greeting not found");
-        }
-        greeting.setId(id);
-        return greetingRepository.save(greeting);
-    }
-
-    public void deleteById(long id) {
-        if (!greetingRepository.existsById(id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Greeting not found");
-        }
-        greetingRepository.deleteById(id);
     }
 }
